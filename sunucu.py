2cb3b36535b8342b0c2f1817190e399974032aa9
Onur Karabulut <39680969+onurkarabulut@users.noreply.github.com>
2021-06-15 17:50:30 +0300

    Add files via upload

---
+++ sunucu.py
@@ -0,0 +1,20 @@
+#Onur Karabulut
+import socket
+import datetime
+
+sct = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+host = ""
+port = 142
+buf = 1024
+UTC = "UTC+3" # UTC değeri buradan degistirilebilir
+sct.bind((host, port))
+sct.listen(1)
+
+
+while True:
+	connection, who = sct.accept()
+	print("Baglanan Istemci IP: {0}".format(who[0]))  
+	time_now = datetime.datetime.now().timestamp() * 1000
+	time_now_utc = str(time_now) + "  " + UTC
+	connection.send(time_now_utc.encode())   
+sct.close()
